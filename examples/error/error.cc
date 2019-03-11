16549523a41ed52a0dd78ded3455813035b401f9
Natanael Rabello <natanael.rabello@outlook.com>
2019-03-10 21:59:25 -0300

    WIP: C++17 enum size of up to 1-byte
    Signed-off-by: Natanael Rabello <natanael.rabello@outlook.com>

--- examples/error/error.cc
+++ examples/error/error.cc
@@ -15,12 +15,12 @@
 /**
  * Error code enumeration
  */
-enum class Error {
+enum class Error : char {
     Ok = 0,
-    Fail = -1,
-    Timeout = -2,
-    Internal = -3,
-    InvalidParam = -4,
+    Fail = 1,
+    Timeout = -1,
+    Internal = 127,
+    InvalidParam = -128,
 };
 
 /***************************************************************************************/
@@ -63,33 +63,91 @@ Error foo(mxl::Enum<Error> err)
 /***************************************************************************************/
 
 template<typename E>
-constexpr bool is_valid_enum(int x)
+constexpr bool is_valid_enum(typename std::underlying_type<E>::type v)
 {
-    return mxl::make_enum(static_cast<E>(x)).name() != nullptr;
+    return mxl::Enum<E>(static_cast<E>(v)).name() != nullptr;
 }
 
+// template<typename E>
+// constexpr int enum_size_impl()
+// {
+//     return 0;
+// }
+
+// template<typename E, typename... N>
+// constexpr int enum_size_impl(typename std::underlying_type<E>::type i, N... n)
+// {
+//     return (is_valid_enum<E>(i) ? 1 : 0) + enum_size_impl<E>(n...);
+// }
+
+// template<typename E, typename std::underlying_type<E>::type... N>
+// constexpr int enum_size_impl(
+//     std::integer_sequence<typename std::underlying_type<E>::type, N...>)
+// {
+//     return enum_size_impl<E>(N...);
+// }
+
+// template<typename E>
+// constexpr int enum_size()
+// {
+//     using enum_type = typename std::underlying_type<E>::type;
+//     static_assert(1 == sizeof(enum_type), "Only enumerations of 1-byte size
+//     supported"); return enum_size_impl<E>(std::make_integer_sequence<enum_type,
+//     127>{});
+// }
+
+template<typename E, typename std::underlying_type<E>::type... N>
+constexpr size_t enum_length_impl_neg(
+    std::integer_sequence<typename std::underlying_type<E>::type, N...>)
+{
+    return ((is_valid_enum<E>((-static_cast<typename std::make_signed<
+                                   typename std::underlying_type<E>::type>::type>(N)) -
+                              1)
+                 ? 1
+                 : 0) +
+            ...);
+}
+
+template<typename E, typename std::underlying_type<E>::type... N>
+constexpr size_t enum_length_impl(
+    std::integer_sequence<typename std::underlying_type<E>::type, N...>)
+{
+    return ((is_valid_enum<E>(N) ? 1 : 0) + ...);
+}
+
+template<typename E>
+constexpr size_t enum_length()
+{
+    using enum_type = typename std::underlying_type<E>::type;
+    static_assert(1 == sizeof(enum_type), "Only enumerations of 1-byte size supported");
+    constexpr enum_type kMax = (1 << ((sizeof(enum_type) * 8) - 1)) - 1;
+    std::make_integer_sequence<enum_type, kMax> seq{};
+    size_t size = enum_length_impl<E>(seq) + (is_valid_enum<E>(kMax) ? 1 : 0);
+    if (std::is_signed<enum_type>::value)
+        size += enum_length_impl_neg<E>(seq) + (is_valid_enum<E>(kMax) ? 1 : 0);
+    return size;
+}
 
 int main()
 {
-    for (int x : { 0, -1, -2, -3, -4 }) {
-        mxl::Enum<Error> errc = function1(x);
-        std::cout << "function1 (" << errc.value() << "): " << errc.name() << std::endl;
-    }
+    // for (int x : { 0, -1, -2, -3, -4 }) {
+    //     mxl::Enum<Error> errc = function1(x);
+    //     std::cout << "function1 (" << errc.value() << "): " << errc.name() <<
+    //     std::endl;
+    // }
 
-    for (int y : { 0, -1, -2, -3, -4 }) {
-        Error errc = function2(y);
-        std::cout << "function2 (" << mxl::make_enum(errc).value()
-                  << "): " << mxl::make_enum(errc).name() << std::endl;
-    }
+    // for (int y : { 0, -1, -2, -3, -4 }) {
+    //     Error errc = function2(y);
+    //     std::cout << "function2 (" << mxl::make_enum(errc).value()
+    //               << "): " << mxl::make_enum(errc).name() << std::endl;
+    // }
 
-    std::cout << "sizeof(Error): " << sizeof(Error) << std::endl;
-    std::cout << "sizeof(mxl::Enum<Error>): " << sizeof(mxl::Enum<Error>) << std::endl;
+    // std::cout << "sizeof(Error): " << sizeof(Error) << std::endl;
+    // std::cout << "sizeof(mxl::Enum<Error>): " << sizeof(mxl::Enum<Error>) << std::endl;
 
     // std::cout << "foo: " << mxl::make_enum(foo(Error::Ok)).name() << std::endl;
 
-    std::make_index_sequence<sizeof(int)> x;
-    bool is = is_valid_enum<Error>(-5);
-    std::cout << "is_valid_enum: " << is << std::endl;
+    std::cout << "enum_length: " << enum_length<Error>() << std::endl;
 
     return 0;
 }
