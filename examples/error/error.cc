e7c3fcd630c3b7772bd80a3540d340f3e36a0e06
Natanael Rabello <natanael.rabello@outlook.com>
2019-03-13 21:36:04 -0300

    WIP: closer to getting a dynamic array
    Signed-off-by: Natanael Rabello <natanael.rabello@outlook.com>

--- examples/error/error.cc
+++ examples/error/error.cc
@@ -18,7 +18,7 @@
  */
 enum class Error : char {
     Ok = 0,
-    Fail = 1,
+    Fail = 2,
     Timeout = -1,
     Internal = 127,
     InvalidParam = -128,
@@ -63,47 +63,113 @@ Error foo(mxl::Enum<Error> err)
 
 /***************************************************************************************/
 
-template<typename E>
-constexpr bool is_valid_enum(typename std::underlying_type<E>::type v)
+template<typename E, typename std::underlying_type<E>::type V>
+struct is_enum_valid {
+    static inline constexpr bool value =
+        mxl::Enum<E>(static_cast<E>(V)).name() != nullptr;
+};
+
+// template<typename E, int I, int... N>
+// struct enum_seq {
+//     using type =
+//         std::array<int, is_enum_valid<E, I>::value ? (sizeof...(N) + 1) :
+//         sizeof...(N)>;
+//     static inline constexpr type value = { N... };
+// };
+
+template<size_t S, size_t... I>
+constexpr auto array_new_add(int x, const std::array<int, S>& a,
+                             std::index_sequence<I...>)
 {
-    return mxl::Enum<E>(static_cast<E>(v)).name() != nullptr;
+    return std::array<int, S + 1>{ a[ I ]..., x };
 }
 
-// template<typename E>
-// constexpr int enum_size_impl()
+template<typename E, size_t S>
+constexpr auto enum_make_array(const std::array<int, S>& a)
+{
+    return a;
+}
+
+template<typename E, size_t S, int I, int... N>
+constexpr auto enum_make_array(const std::array<int, S>& a) ->
+    typename std::enable_if<!is_enum_valid<E, I>::value, std::array<int, S>>::type
+{
+    return enum_make_array<E, S, N...>(a);
+}
+
+template<typename E, size_t S, int I, int... N>
+constexpr auto enum_make_array(const std::array<int, S>& a) ->
+    typename std::enable_if<is_enum_valid<E, I>::value, std::array<int, S + 1>>::type
+{
+    return enum_make_array<E, S + 1, N...>(
+        array_new_add(I, a, std::make_index_sequence<S>{}));
+}
+
+template<typename E, int... N>
+constexpr auto resolve_impl(std::integer_sequence<int, N...>)
+{
+    return enum_make_array<E, N...>(std::array<int, 0>{});
+}
+
+template<typename E>
+constexpr auto resolve()
+{
+    return resolve_impl<E>(std::make_integer_sequence<int, 127>{});
+}
+
+auto x = resolve<Error>();
+
+// template<typename E, int... N>
+// constexpr auto enum_compose_impl(int I, )
 // {
-//     return 0;
+//     return enum_compose_impl
 // }
 
-// template<typename E, typename... N>
-// constexpr int enum_size_impl(typename std::underlying_type<E>::type i, N... n)
-// {
-//     return (is_valid_enum<E>(i) ? 1 : 0) + enum_size_impl<E>(n...);
-// }
+template<typename E, size_t... N>
+constexpr auto enum_compose_impl(std::index_sequence<N...>)
+{
+}
 
-// template<typename E, typename std::underlying_type<E>::type... N>
-// constexpr int enum_size_impl(
-//     std::integer_sequence<typename std::underlying_type<E>::type, N...>)
-// {
-//     return enum_size_impl<E>(N...);
-// }
+template<typename E>
+constexpr auto enum_compose()
+{
+    return enum_compose_impl<E>(std::make_index_sequence<127>{});
+}
 
-// template<typename E>
-// constexpr int enum_size()
-// {
-//     using enum_type = typename std::underlying_type<E>::type;
-//     static_assert(1 == sizeof(enum_type), "Only enumerations of 1-byte size
-//     supported"); return enum_size_impl<E>(std::make_integer_sequence<enum_type,
-//     127>{});
-// }
+template<typename E>
+constexpr int enum_size_impl()
+{
+    return 0;
+}
+
+template<typename E, typename... N>
+constexpr int enum_size_impl(typename std::underlying_type<E>::type i, N... n)
+{
+    return (is_enum_valid<E, i>::value ? 1 : 0) + enum_size_impl<E>(n...);
+}
+
+template<typename E, typename std::underlying_type<E>::type... N>
+constexpr int enum_size_impl(
+    std::integer_sequence<typename std::underlying_type<E>::type, N...>)
+{
+    return enum_size_impl<E>(N...);
+}
+
+template<typename E>
+constexpr int enum_size()
+{
+    using enum_type = typename std::underlying_type<E>::type;
+    static_assert(1 == sizeof(enum_type), "Only enumerations of 1-byte size supported ");
+    return enum_size_impl<E>(std::make_integer_sequence<enum_type, 127>{});
+}
 
 template<typename E, typename std::underlying_type<E>::type... N>
 constexpr auto enum_array_impl_neg(
     std::integer_sequence<typename std::underlying_type<E>::type, N...>)
 {
-    return ((is_valid_enum<E>((-static_cast<typename std::make_signed<
+    return ((is_enum_valid<E, (-static_cast<typename std::make_signed<
                                    typename std::underlying_type<E>::type>::type>(N)) -
-                              1)
+                                  1>::value
                  ? 1
                  : 0) +
             ...);
@@ -130,7 +196,7 @@ constexpr auto enum_array_impl(
     std::integer_sequence<typename std::underlying_type<E>::type, N...>)
 {
     std::array<int, 0> a;
-    return ((is_valid_enum<E>(N) ? N : 0) << ...);
+    return ((is_enum_valid<E, N>::value ? N : 0) << ...);
 }
 
 template<typename E>
@@ -140,10 +206,10 @@ constexpr auto enum_array()
     static_assert(1 == sizeof(enum_type), "Only enumerations of 1-byte size supported");
     constexpr enum_type kMax = (1 << ((sizeof(enum_type) * 8) - 1)) - 1;
     std::make_integer_sequence<enum_type, kMax> seq{};
-    // size_t size = enum_array_impl<E>(seq) + (is_valid_enum<E>(kMax) ? 1 : 0);
+    // size_t size = enum_array_impl<E>(seq) + (is_enum_valid<E>(kMax) ? 1 : 0);
     auto array = enum_array_impl<E>(seq);
     // if (std::is_signed<enum_type>::value)
-    //     size += enum_array_impl_neg<E>(seq) + (is_valid_enum<E>(kMax) ? 1 : 0);
+    //     size += enum_array_impl_neg<E>(seq) + (is_enum_valid<E>(kMax) ? 1 : 0);
     return array;
 }
 
@@ -166,13 +232,17 @@ int main()
 
     // std::cout << "foo: " << mxl::make_enum(foo(Error::Ok)).name() << std::endl;
 
-    std::array<int, 2> a{4, 8};
+    std::array<int, 2> a{ 4, 8 };
     auto b = a << 1;
     auto c = b << -9;
     for (auto i : c)
         std::cout << i << " ";
 
     auto ea = enum_array<Error>();
+
+    std::cout << std::endl;
+    for (auto i : x)
+        std::cout << i << " ";
 
     std::cout << std::endl;
     return 0;
